8fa47063846c05e9c74fb981a66391d299f5d5c8
piomin <piotr.minkowski@gmail.com>
2023-03-28 14:45:55 +0200

    update vault conf

--- vault-ocp/vault-provider-sb/main.tf
+++ vault-ocp/vault-provider-sb/main.tf
@@ -8,38 +8,31 @@ provider "vault" {
   address = var.vault-addr
 }
 
-resource "vault_auth_backend" "kubernetes" {
-  type       = "kubernetes"
+data "vault_auth_backend" "kubernetes" {
+  path       = "kubernetes"
 }
 
-data "kubernetes_service_account" "vault-sa" {
+data "kubernetes_secret" "vault-token" {
   metadata {
-    name      = "vault"
-    namespace = "vault"
-  }
-}
-
-data "kubernetes_secret" "vault-default" {
-  metadata {
-    name      = data.kubernetes_service_account.vault-sa.default_secret_name
+    name      = "vault-token"
     namespace = "vault"
   }
 }
 
 resource "vault_kubernetes_auth_backend_config" "example" {
-  backend                = vault_auth_backend.kubernetes.path
+  backend                = data.vault_auth_backend.kubernetes.path
   kubernetes_host        = "https://172.30.0.1:443"
-  kubernetes_ca_cert     = data.kubernetes_secret.vault-default.data["ca.crt"]
-  token_reviewer_jwt     = data.kubernetes_secret.vault-default.data.token
+  kubernetes_ca_cert     = data.kubernetes_secret.vault-token.data["ca.crt"]
+  token_reviewer_jwt     = data.kubernetes_secret.vault-token.data.token
 }
 
 resource "vault_kubernetes_auth_backend_role" "webapp-role" {
-  backend                          = vault_auth_backend.kubernetes.path
+  backend                          = data.vault_auth_backend.kubernetes.path
   role_name                        = "webapp"
   bound_service_account_names      = ["webapp"]
   bound_service_account_namespaces = ["webapp"]
   token_ttl                        = 3600
-  token_policies                   = ["argocd"]
+  token_policies                   = ["argocd", "internal-es"]
 }
 
 resource "vault_mount" "kv-v2" {
@@ -54,6 +47,16 @@ resource "vault_policy" "read-key" {
 
   policy = <<EOT
 path "kv-v2/data/argocd" {
+  capabilities = ["read"]
+}
+EOT
+}
+
+resource "vault_policy" "internal-es" {
+  name = "internal-es"
+
+  policy = <<EOT
+path "secret/data/example" {
   capabilities = ["read"]
 }
 EOT
